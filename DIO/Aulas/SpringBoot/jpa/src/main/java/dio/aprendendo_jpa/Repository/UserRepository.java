7919b02268c9f805cf4cebb934ee61b4ee270301
K4rlous <carlloshenrique.1971@gmail.com>
2025-05-12 21:05:25 -0300

    adição textual

--- DIO/Aulas/SpringBoot/jpa/src/main/java/dio/aprendendo_jpa/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dio.aprendendo_jpa.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dio.aprendendo_jpa.Model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{ // <- Usaremos o repositório User e o id dele é Integer
-    
-}
